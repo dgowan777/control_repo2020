415cf3876cd8538aa15be99e44cb7cd2904e2a3c
dgowan777 <58914160+dgowan777@users.noreply.github.com>
2020-07-16 15:08:51 -0700

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,8 @@ class profile::base {
   user {'admin':
     ensure => present,
   }
+  file { '/root/facter_file':
+    ensure => file,
+    content => "Host $fqdn\nOS ${os[name]}\nVersion ${os[release][full]}\nKernel $kernelversion\n",
+  }
 }
