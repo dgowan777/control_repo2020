a85a97868068f4a777d12c7c39ce48e5fe0327ec
dgowan777 <58914160+dgowan777@users.noreply.github.com>
2020-07-16 15:07:36 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,6 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README':
-    ensure => file,
-    content => "hostname $fqdn\nos ${os[name]}\nkernel $kernelversion\n",
-  }
 }
 node /^web/ {
   include role::app_server
