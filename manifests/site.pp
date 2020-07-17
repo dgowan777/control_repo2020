e6bc967f1814a965e257f03a21b24194e226afca
dgowan777 <58914160+dgowan777@users.noreply.github.com>
2020-07-17 08:57:55 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,18 @@
 node default {
 }
+
 node 'master.puppet.vm' {
   include role::master_server
 }
+
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
+
 node /^db/ {
   include role::db_server
 }
