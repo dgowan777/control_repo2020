05b9d89240f626890a483ab561cf51c5172d53d6
dgowan777 <58914160+dgowan777@users.noreply.github.com>
2020-07-17 09:19:03 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,6 +6,7 @@ class minecraft {
   file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
+    before => Service['minecraft'],
   }
   
   package {'java':
@@ -25,5 +26,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service'],
   }
 }
